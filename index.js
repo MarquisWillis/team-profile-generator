d64bd960f0e1ee0f2314e695881841b36eb2bbce
Marquis Willis <marquiswillis99@gmail.com>
2022-07-18 22:37:30 -0400

    added several functions to index.js (init, confirmNext, addEmployee, createHTML), the question arrays for each role, and incorperated generateFile functions into index to output html into dist

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+const generateManager = require('./src/generateManager');
+const generateEngineer = require('./src/generateEngineer');
+const generateIntern = require('./src/generateIntern');
+
 const generateHTML = require('./src/generateHTML');
 
 const inquirer = require('inquirer');
@@ -22,28 +26,120 @@ const fs = require('fs');
         - if yes, loop thru instructions above all over again and add as many team members as you want
         - otherwise, generate html and css output in the dist folder
 */
-const nextArray = ['Engineer', 'Intern', 'Neither'];
-const roleQuestionArray = [{type: 'list', message: 'Would you like to add an engineer, and intern, or neither?', choices: nextArray, name: "roleName"}];
+const employeeArray = [];
 
 const managerQuestions = [
-    {type: "input", message: "What is the name?", name: "managerName"}, 
-    {type: "input", message: "What is the id?", name: "managerId"},
-    {type: "input", message: "What is the email?", name: "managerEmail"},
-    {type: "input", message: "What is the office number?", name: "managerNumber"}
+    { type: "input", message: "What is the name?", name: "managerName" },
+    { type: "input", message: "What is the id?", name: "managerId" },
+    { type: "input", message: "What is the email?", name: "managerEmail" },
+    { type: "input", message: "What is the office number?", name: "managerNumber" }
 ];
 
 const engineerQuestions = [
-    {type: "input", message: "What is the name?", name: "engineerName"}, 
-    {type: "input", message: "What is the id?", name: "engineerId"},
-    {type: "input", message: "What is the email?", name: "engineerEmail"},
-    {type: "input", message: "What is the office number?", name: "engineerNumber"}
+    { type: "input", message: "What is the name?", name: "engineerName" },
+    { type: "input", message: "What is the id?", name: "engineerId" },
+    { type: "input", message: "What is the email?", name: "engineerEmail" },
+    { type: "input", message: "What is the github?", name: "engineerGithub" }
 ];
 
 const internQuestions = [
-    {type: "input", message: "What is the name?", name: "internName"}, 
-    {type: "input", message: "What is the id?", name: "internId"},
-    {type: "input", message: "What is the email?", name: "internEmail"},
-    {type: "input", message: "What is the office number?", name: "internNumber"}
+    { type: "input", message: "What is the name?", name: "internName" },
+    { type: "input", message: "What is the id?", name: "internId" },
+    { type: "input", message: "What is the email?", name: "internEmail" },
+    { type: "input", message: "What is the school?", name: "internSchool" }
 ];
 
 // 3.) link src helper files to generate team profile
+
+function init() {
+    inquirer
+        .prompt(managerQuestions)
+        .then(response => {
+            const manager = new Manager
+                (response.managerName,
+                    response.managerId,
+                    response.managerEmail,
+                    response.managerNumber);
+
+            employeeArray.push(manager);
+            confirmNext();
+        })
+};
+
+function confirmNext() {
+    inquirer
+        .prompt([{
+            type: 'confirm',
+            message: 'Do you want to continue to add employees to your team?',
+            name: 'addMore'
+        }])
+        .then(response => {
+            if (response.addMore === true) {
+                addEmployee();
+            } else {
+                createHTML();
+            }
+        })
+};
+
+function addEmployee() {
+    inquirer
+        .prompt([{
+            type: 'list',
+            message: 'Do you wish to add an engineer or an intern?',
+            choices: ["Engineer", "Intern"],
+            name: 'addType'
+        }])
+        .then((response) => {
+            if (response.addType === 'Engineer') {
+                inquirer
+                    .prompt(engineerQuestions)
+                    .then(response => {
+                        const engineer = new Engineer
+                        (response.engineerName,
+                            response.engineerId,
+                            response.engineerEmail,
+                            response.engineerNumber);
+    
+                    employeeArray.push(engineer);
+                    confirmNext();
+                })
+                
+            } else if (response.addType === 'Intern'){
+                inquirer
+                    .prompt(internQuestions)
+                    .then(response => {
+                        const intern = new Intern
+                        (response.internName,
+                            response.internId,
+                            response.internEmail,
+                            response.internNumber);
+    
+                    employeeArray.push(intern);
+                    confirmNext();
+                })
+        }
+    })
+}
+
+function createHTML() {
+    console.log(employeeArray)
+
+    let cards = ""
+
+    for (let i = 0; i < employeeArray.length; i++) {
+        if (employeeArray[i].getRole() === "Manager") {
+            cards = cards + generateManager(employeeArray[i]);
+        }
+        else if (employeeArray[i].getRole() === "Engineer") {
+            //same as manager card but for Enineer card
+            cards = cards + generateEngineer(employeeArray[i]);
+        } else {
+            //same as manager card but for  intern card
+            cards = cards + generateIntern(employeeArray[i]);
+        }
+    }
+    fs.writeFileSync("./dist/team.html", generateHTML(cards));
+}
+
+init();
---
+++ src/generateManager.js
@@ -0,0 +1,18 @@
+function generateManager(manager) {
+    return `  <div class="col-sm-4 my-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Manger</h5>
+        <ul class="list-group">
+          <li class="list-group-item">name: ${manager.name}</li>
+          <li class="list-group-item">id:  ${manager.id}</li>
+          <li class="list-group-item">email: ${manager.email}</li>
+          <li class="list-group-item">OfficeNumber: ${manager.officeNumber}</li>
+           
+        </ul>
+      </div>
+    </div>
+  </div>`
+};
+
+module.exports = generateManager;
